eccb3cf1d3dc5dee7abc965f9157fd9eab58bb65
ilmagnificho <jyj0520@gmail.com>
2026-02-10 15:52:41 +0900

    fix: Move ToastProvider to RootLayout to cover all pages

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import { Sidebar } from '@/components/layout/sidebar';
-import { ToastProvider } from '@/components/ui/toast';
 
 export const dynamic = 'force-dynamic';
 
@@ -13,9 +12,7 @@ export default function DashboardLayout({
             <Sidebar />
             <div className="flex-1 flex flex-col md:pl-64 transition-all duration-300">
                 <main className="flex-1 p-6 md:p-8 pt-20 md:pt-8 max-w-6xl mx-auto w-full">
-                    <ToastProvider>
-                        {children}
-                    </ToastProvider>
+                    {children}
                 </main>
             </div>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import { ToastProvider } from "@/components/ui/toast";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -25,7 +26,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <ToastProvider>
+            {children}
+          </ToastProvider>
         </ThemeProvider>
       </body>
     </html>
